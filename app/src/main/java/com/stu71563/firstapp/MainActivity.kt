7425d1154496c36b38111e6e2afc531cb1cdd4e7
Juantrevi <juantrevi70@gmail.com>
2024-02-03 23:23:56 +0000

    -- Changing app --

--- app/src/main/java/com/stu71563/firstapp/MainActivity.kt
+++ app/src/main/java/com/stu71563/firstapp/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.stu71563.firstapp.ui.theme.FirstAppTheme
@@ -34,8 +33,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma"
+                        message = "Happy Birthday Juan!",
+                        from = "From Juan"
                     )
                 }
             }
