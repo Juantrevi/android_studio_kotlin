38bddc310dd26174466d47e5c193c390d001b146
Juantrevi <juantrevi70@gmail.com>
2024-02-03 20:46:41 +0000

    -- Changing app --

--- app/src/main/java/com/stu71563/firstapp/MainActivity.kt
+++ app/src/main/java/com/stu71563/firstapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.stu71563.firstapp
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.stu71563.firstapp.ui.theme.FirstAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,36 +24,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Birthday Sam!", from = "From: Stuart")
                 }
             }
         }
     }
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//            text = "Hello $name!",
-//            modifier = modifier
-//    )
-//}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    FirstAppTheme {
+        GreetingText(message = "Happy Birthday Sam!", from = "From: Stuart")
+    }
+}
 
 /*
 * @Composable
 * is a function that defines a UI component.
 * It is a function that takes in data and returns a UI component.
 * */
-@Preview(showBackground = true)
+/*
+* GreetingText()
+* function displays text in the UI. It does so by calling the Text() composable function.
+* */
 @Composable
-fun BirthdayCardPreview() {
-    FirstAppTheme {
-        BirthdayCard(message = "Happy Birthday!")
-    }
-}
-
-@Composable
-fun BirthdayCard(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+        )
     Text(
-        text = "Happy Birthday!"
+        text = from,
+        fontSize = 36.sp,
     )
+    }
 }
